6a0bbe54b4653643ba438cff78e22088e93c9df0
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-06-13 19:45:11 +0300

    add App component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,16 +1,51 @@
-import React from 'react';
+import React from "react"
+import Card from "./Card"
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div id="content">
-        <h1>&nbsp;</h1>
-        <h2>Welcome!</h2>
-        <ul>
-          <li><a href="http://brunch.io">Brunch homepage</a></li>
-          <li><a href="https://facebook.github.io/react/">React.js homepage</a></li>
-        </ul>
-      </div>
-    );
-  }
+const api = "http://pokeapi.co"
+
+export default class App extends React.Component
+{
+    constructor()
+    {
+        super(...arguments)
+
+        this.loadPokemons = this.loadPokemons.bind(this)
+        this.state =
+        {
+            pokemons: [],
+            next: "/api/v1/pokemon/?limit=12"
+        }
+    }
+
+    loadPokemons()
+    {
+        let {next, pokemons} = this.state
+        this.setState({ loading: true })
+
+        fetch(api + next)
+            .then(response => response.json())
+            .then(json =>
+            {
+                let {meta: {next}, objects} = json
+
+                pokemons.push(...objects)
+                this.setState({ next, loading: false })
+            })
+    }
+
+    render()
+    {
+        let {pokemons, loading} = this.state
+        let cards = pokemons.map(data =>
+            <Card {...data} key={data.pkdx_id} />
+        )
+
+        return <div>
+            <ul>{cards}</ul>
+            <button onClick={this.loadPokemons}>
+                <progress hidden={!loading} />
+                Load more
+            </button>
+        </div>
+    }
 }
