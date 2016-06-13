eb8977586ab45e32a4017870e1398bd5d78ca0c2
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-06-13 20:41:57 +0300

    display types in card

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-const images = "http://pokeapi.co/media/img/"
+const url = "http://pokeapi.co"
 
 export default class Card extends React.Component
 {
@@ -12,11 +12,15 @@ export default class Card extends React.Component
     render()
     {
         let {pkdx_id, name} = this.props
-        let image = `${images}/${pkdx_id}.png`
+        let image = `${url}/media/img/${pkdx_id}.png`
+        let types = this.props.types.map(({name}) =>
+            <li key={name}>{name}</li>
+        )
 
         return <div>
             <img src={image} alt={name} />
             <h1>{name} #{pkdx_id}</h1>
+            <ul>{types}</ul>
         </div>
     }
 }
